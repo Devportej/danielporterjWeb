ffb438d96f00d4db9e15277dce0544f68cd553dd
Devportej <danielporterj@gmail.com>
2024-11-24 13:19:57 -0500

    feat: add projects page

---
+++ src/Projects/Projects.tsx
@@ -0,0 +1,15 @@
+import Card from '../components/Card/Card';
+import './Projects.css';
+
+const Projects = () => {
+    return (
+      <div className="projects">
+        <h1>Welcome to Projects</h1>
+        <Card>
+          <h2>Hello</h2>
+        </Card>
+      </div>
+    );
+}
+
+export default Projects;
